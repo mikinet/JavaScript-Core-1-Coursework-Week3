fa9cc771cdda6a8eebe10ceff2a4b891ef3fedf5
mikinet <antenehnigussie1@yahoo.com>
2020-12-22 16:59:00 +0000

    Updated files to complete the exericses.

--- 1-exercises/E-array-map/exercise.js
+++ 1-exercises/E-array-map/exercise.js
@@ -2,3 +2,29 @@
 // Write multiple solutions using different syntax (as shown in the README)
 
 var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
+
+var times100;
+
+function multiplyBy100(num) {
+    return num * 100;
+}
+times100 = numbers.map(multiplyBy100);
+console.log("Result using conventional method: ", times100);
+
+times100 = numbers.map(function multiplyBy100(num) {
+    return num * 100;
+});
+console.log("Result using anonymous function: ", times100);
+
+times100 = numbers.map(function (num) {
+    return num * 100;
+});
+console.log("Result using anonymous function (simplified): ", times100);
+
+times100 = numbers.map(num => {
+    return num * 100;
+});
+console.log("Result using arrow function: ", times100);
+
+times100 = numbers.map(num => num * 100);
+console.log("Result using arrow function (simplified): ",times100);
