5ced74a9934b5bb5b6f1c545b33776068f86c606
mikinet <antenehnigussie1@yahoo.com>
2020-12-23 11:59:37 +0000

    Missed algorithm challenge completed.

--- 3-extra/2-sorting-algorithm.js
+++ 3-extra/2-sorting-algorithm.js
@@ -14,7 +14,47 @@ You don't have to worry about making this algorithm work fast! The idea is to ge
 "think" like a computer and practice your knowledge of basic JavaScript.
 */
 
-function sortAges(arr) {}
+// Function to sort an array containing numbers representing people's ages
+function sortAges(arr) {
+  // test if arr is actually an array
+  let isArray = Array.isArray(arr);
+  if (isArray === true) { // if arr is actually an array
+    let filteredArr = [];  // declare a variable to store only numeric values filtered from arr
+    // filter numeric values from arr
+    for (let i = 0; i < arr.length; i++){
+      if (typeof arr[i] === "number") {
+        filteredArr.push(arr[i]);
+      }
+    }
+    // now arr is filtered, check if filteredArr is not empty
+    let isNotEmpty = filteredArr.length > 0;
+    if (isNotEmpty === true) {  // if filteredArr has elements in it
+
+      /*****************  START SORTING ******************/
+      // to sort the filteredArr is the correct order, compare a number with every other number
+        // in the unordered section of filteredArr. 
+      for (let i = 0; i < filteredArr.length; i++){ // do this for every number in the list, starting from the first one
+        let largest = filteredArr[i]; // simplify expression. Assume largest number to be the current number under test
+        // let pos = i+1;  // position (index) of the first number we start to compare largest with  
+        // start comparing
+        for (let j = i+1; j < filteredArr.length; j++){ // scan through the list  
+          let otherNum = filteredArr[j];  // simplify expression
+          if (largest > otherNum) { // if largest is greater than the number we are comparing it with            
+            // swap 
+            filteredArr[i] = otherNum;
+            filteredArr[j] = largest;
+            largest = otherNum;  // update largest number
+          }
+        }
+      /******************* FINISH SORTING *****************/
+      }
+      return filteredArr; // send back the sorted version of arr to caller
+    } else {  // if filteredArray is empty
+      return; // exit program without doing anything
+    }
+  }
+  
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
